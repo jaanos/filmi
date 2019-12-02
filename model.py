f796de2aa65a3b03492935d7fa6b51acc6ca567d
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2019-12-02 01:53:11 +0100

    Dodana dokumentacija razredov in funkcij

--- model.py
+++ model.py
@@ -8,61 +8,96 @@ conn.execute('PRAGMA foreign_keys = ON')
 
 
 class Film:
+    """
+    Razred za film.
+    """
+
     def __init__(self, id, naslov, leto, ocena):
+        """
+        Konstruktor filma.
+        """
         self.id = id
         self.naslov = naslov
         self.leto = leto
         self.ocena = ocena
 
     def __str__(self):
+        """
+        Znakovna predstavitev filma.
+
+        Vrne naslov filma.
+        """
         return self.naslov
 
     @staticmethod
     def najboljsi_v_letu(leto):
-        '''Vrne najboljših 10 filmov v danem letu.'''
-        sql = '''
+        """
+        Vrne najboljših 10 filmov v danem letu.
+        """
+        sql = """
             SELECT id, naslov, leto, ocena
             FROM film
             WHERE leto = ?
             ORDER BY ocena DESC
             LIMIT 10
-        '''
+        """
         for id, naslov, leto, ocena in conn.execute(sql, [leto]):
             yield Film(id, naslov, leto, ocena)
 
 
 class Oseba:
+    """
+    Razred za osebo.
+    """
+
     def __init__(self, id, ime):
+        """
+        Konstruktor osebe.
+        """
         self.id = id
         self.ime = ime
 
     def __str__(self):
+        """
+        Znakovna predstavitev osebe.
+
+        Vrne ime osebe.
+        """
         return self.ime
 
     def poisci_vloge(self):
-        sql = '''
+        """
+        Vrne vloge osebe.
+        """
+        sql = """
             SELECT film.naslov, film.leto, vloga.tip
             FROM film
                 JOIN vloga ON film.id = vloga.film
             WHERE vloga.oseba = ?
             ORDER BY leto
-        '''
+        """
         for naslov, leto, tip_vloge in conn.execute(sql, [self.id]):
             yield (naslov, leto, TipVloge[tip_vloge])
 
     @staticmethod
     def poisci(niz):
-        '''Vrne vse osebe, ki v imenu vsebujejo dani niz.'''
-        sql = '''
-            SELECT id, ime FROM oseba WHERE ime LIKE ?
-        '''
+        """
+        Vrne vse osebe, ki v imenu vsebujejo dani niz.
+        """
+        sql = "SELECT id, ime FROM oseba WHERE ime LIKE ?"
         for id, ime in conn.execute(sql, ['%' + niz + '%']):
             yield Oseba(id, ime)
 
 
 class TipVloge(Enum):
+    """
+    Oznake za tip vloge.
+    """
     I = 'igralec'
     R = 'režiser'
 
     def __str__(self):
+        """
+        Znakovna predstavitev tipa vloge.
+        """
         return self.value
--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -3,21 +3,36 @@ from model import Film, Oseba
 
 
 class Meni(Enum):
+    """
+    Razred za izbire v menijih.
+    """
     def __init__(self, ime, funkcija):
+        """
+        Konstruktor izbire.
+        """
         self.ime = ime
         self.funkcija = funkcija
 
     def __str__(self):
+        """
+        Znakovna predstavitev izbire.
+        """
         return self.ime
 
 
 def izpisi_vloge(igralec):
+    """
+    Izpiše vse vloge podanega igralca.
+    """
     print(igralec.ime)
     for naslov, leto, vloga in igralec.poisci_vloge():
         print(f'- {naslov} ({leto}, {vloga})')
 
 
 def vnesi_izbiro(moznosti):
+    """
+    Uporabniku da na izbiro podane možnosti.
+    """
     moznosti = list(moznosti)
     for i, moznost in enumerate(moznosti, 1):
         print(f'{i}) {moznost}')
@@ -26,6 +41,9 @@ def vnesi_izbiro(moznosti):
 
 
 def poisci_osebo():
+    """
+    Poišče osebo, ki jo vnese uporabnik.
+    """
     while True:
         ime_igralca = input('Kdo te zanima? ')
         osebe = list(Oseba.poisci(ime_igralca))
@@ -39,11 +57,17 @@ def poisci_osebo():
 
 
 def iskanje_osebe():
+    """
+    Izpiše vloge za osebo, ki jo vnese uporabnik.
+    """
     oseba = poisci_osebo()
     izpisi_vloge(oseba)
 
 
 def najboljsi_filmi():
+    """
+    Izpiše najboljših 10 filmov v letu, ki ga vnese uporabnik.
+    """
     leto = input('Katero leto te zanima? ')
     filmi = Film.najboljsi_v_letu(leto)
     for mesto, film in enumerate(filmi, 1):
@@ -51,16 +75,25 @@ def najboljsi_filmi():
 
 
 def domov():
+    """
+    Pozdravi pred izhodom.
+    """
     print('Adijo!')
 
 
 class GlavniMeni(Meni):
+    """
+    Izbire v glavnem meniju.
+    """
     ISKAL_OSEBO = ('Iskal osebo', iskanje_osebe)
     POGLEDAL_DOBRE_FILME = ('Pogledal dobre filme', najboljsi_filmi)
     SEL_DOMOV = ('Šel domov', domov)
 
 
 def glavni_meni():
+    """
+    Prikazuje glavni meni, dokler uporabnik ne izbere izhoda.
+    """
     print('Pozdravljen v bazi filmov!')
     while True:
         print('Kaj bi rad delal?')
